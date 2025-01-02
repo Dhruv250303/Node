1ff0b355fa1d988ea0cb6efce329864da79587df
Dhruv Gahlot <dhruvgahlot2503@gmail.com>
2025-01-02 18:23:49 +0530

    tutotrial 1/4 done

---
+++ tutorial/alternativemodules.js
@@ -0,0 +1,6 @@
+module.exports.items = ['Juice', 'Milk']
+const person = {
+  name: 'bob',
+}
+
+module.exports.singlePerson = person
---
+++ tutorial/modules.js
@@ -0,0 +1,10 @@
+// Modules
+
+const sayHi = require("./utils");
+const names = require("./names");
+const data = require('./alternativemodules');
+require('./mind-grenade')
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
+// console.log(data);
